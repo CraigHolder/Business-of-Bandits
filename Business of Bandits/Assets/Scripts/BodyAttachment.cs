ffced55c9b6057efdbef4c9ca70853d5f92b048b
CraigHolder <craig.holder@ontariotechu.net>
2020-10-01 16:53:43 -0400

    Character without ragdoll body

--- Business of Bandits/Assets/Scripts/BodyAttachment.cs
+++ Business of Bandits/Assets/Scripts/BodyAttachment.cs
@@ -12,13 +12,15 @@ public class BodyAttachment : MonoBehaviour
     void Start()
     {
         //TODO fix the collisions to prevent the front colliding with the rest of the body
-        Physics.IgnoreCollision(o_body.GetComponent<Collider>(), TheAttach.GetComponent<Collider>());
+        Physics.IgnoreCollision(o_body.GetComponent<CapsuleCollider>(), TheAttach.GetComponent<CapsuleCollider>());
+        Physics.IgnoreLayerCollision(8, 9, true);
     }
 
     // Update is called once per frame
     void Update()
     {
         TheObj.transform.position = o_body.transform.position;
-        
+        TheObj.transform.eulerAngles = o_body.transform.eulerAngles;
+
     }
 }
